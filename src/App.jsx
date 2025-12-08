0bfb21036ac855dc6af1ba825851bd0269eb7970
David <your-email@example.com>
2025-12-08 13:12:19 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './Pages/Home'
+import { Home } from './Pages/Home.jsx'
 import { ThemeToggle } from './assets/components/ThemeToggle'
 import { NotFound } from './pages/NotFound'  
 
